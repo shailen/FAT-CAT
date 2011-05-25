6848fa2d0b6c0632c97c505e441bae4cc6bdf6d9
Shailen Tuli <shailen.tuli@gmail.com>
2011-05-25 15:05:27 -0700

    Preliminary commit of code to generate msa's for tree nodes

--- fc_rough.py
+++ fc_rough.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from Bio import Phylo, AlignIO
+from Bio.Align import MultipleSeqAlignment
 
 tree_file = "sample_tree.ml"
 msa_file = "sample_alignment.msa"
@@ -16,4 +17,20 @@ sequence_msa_map = {}
 for entry in msa:
     sequence_msa_map[entry.id] = entry
 
+for id, clade in enumerate(tree.find_clades()):
+    clade.our_unique_identifier = id
 
+# this isn't quite done yet.....
+# we need to figure out the issue of assigning unique ids 
+# to internal nodes
+def build_msa(node, sequence_msa_map):
+    key = str(id(node))
+    file = key + '.msa'
+    file_handle = open(file, 'w')
+    terminals = node.get_terminals()
+    alignments = [sequence_msa_map[terminal.name] for terminal in
+            terminals]
+    alignments = MultipleSeqAlignment(alignments)
+    AlignIO.write(alignments, file_handle, 'stockholm')
+
+build_msa(tree.root, sequence_msa_map)
