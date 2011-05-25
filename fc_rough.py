5bea51d36746354895eab37ba085cc5ea6a637b6
Shailen Tuli <shailen.tuli@gmail.com>
2011-05-25 14:14:26 -0700

    Wrote code to map ids to sequence objects.

--- fc_rough.py
+++ fc_rough.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
 
+from Bio import Phylo, AlignIO
+
 tree_file = "sample_tree.ml"
 msa_file = "sample_alignment.msa"
+new_sequence_file = "new_sequence.fasta"
+
+tree = Phylo.read(tree_file, "newick")
+msa = AlignIO.read(msa_file, "fasta")
+
+# Note: the name of the tree leaf nodes is going to be the 
+# same as the id of each msa entry
+
+sequence_msa_map = {}
+for entry in msa:
+    sequence_msa_map[entry.id] = entry
+
+
