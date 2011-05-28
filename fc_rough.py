cb2b809fd4185006676be589b3b9ba2861a032a5
Shailen Tuli <shailen.tuli@gmail.com>
2011-05-27 21:59:23 -0700

    Added code to generate MSAs for each node in a tree

--- fc_rough.py
+++ fc_rough.py
@@ -1,36 +1,47 @@
 #!/usr/bin/python
+# Shailen and Grant's Excellent Adventure
 
 from Bio import Phylo, AlignIO
 from Bio.Align import MultipleSeqAlignment
 
-tree_file = "sample_tree.ml"
-msa_file = "sample_alignment.msa"
-new_sequence_file = "new_sequence.fasta"
 
-tree = Phylo.read(tree_file, "newick")
-msa = AlignIO.read(msa_file, "fasta")
-
-# Note: the name of the tree leaf nodes is going to be the 
-# same as the id of each msa entry
-
-sequence_msa_map = {}
-for entry in msa:
-    sequence_msa_map[entry.id] = entry
-
-for id, clade in enumerate(tree.find_clades()):
-    clade.our_unique_identifier = id
-
-# this isn't quite done yet.....
-# we need to figure out the issue of assigning unique ids 
-# to internal nodes
 def build_msa(node, sequence_msa_map):
-    key = str(id(node))
-    file = key + '.msa'
-    file_handle = open(file, 'w')
+    key = str(node.__hash__())
+    file_name = key + '.msa'
+    file_handle = open(file_name, 'w')
     terminals = node.get_terminals()
     alignments = [sequence_msa_map[terminal.name] for terminal in
             terminals]
     alignments = MultipleSeqAlignment(alignments)
     AlignIO.write(alignments, file_handle, 'stockholm')
+    return file_name
 
-build_msa(tree.root, sequence_msa_map)
+def treewalker(root, sequence_msa_map):
+    msa_file_name = build_msa(root, sequence_msa_map)
+    for clade in root.clades:
+        treewalker(clade, sequence_msa_map)
+    return
+
+def main():
+    """Shailen and Grant's totally radical main function."""
+    
+    tree_file = "sample_tree.ml"
+    msa_file = "sample_alignment.msa"
+    new_sequence_file = "new_sequence.fasta"
+    
+    # Note: the name of the tree leaf nodes is going to be the 
+    # same as the id of each msa entry
+
+    tree = Phylo.read(tree_file, "newick")
+    msa = AlignIO.read(msa_file, "fasta")
+    
+    sequence_msa_map = {}
+    for entry in msa:
+        sequence_msa_map[entry.id] = entry
+
+    print tree
+    
+    treewalker(tree.root, sequence_msa_map)
+    
+if __name__ == '__main__':
+    main()
